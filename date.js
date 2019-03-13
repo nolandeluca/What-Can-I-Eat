68a14e3f85edce9a8a73e160e11daf5992abec81
Bradan626 <bradan626@gmail.com>
2019-03-13 15:09:25 -0400

    formatted

--- date.js
+++ date.js
@@ -1,16 +1,14 @@
 let today = new Date();
 let dd = today.getDate();
 
-let mm = today.getMonth()+1; 
+let mm = today.getMonth() + 1;
 const yyyy = today.getFullYear();
-if(dd<10) 
-{
-    dd=`0${dd}`;
-} 
+if (dd < 10) {
+  dd = `0${dd}`;
+}
 
-if(mm<10) 
-{
-    mm=`0${mm}`;
-} 
+if (mm < 10) {
+  mm = `0${mm}`;
+}
 today = `${yyyy}-${mm}-${dd}`;
 console.log(today);
--- foodfriend/src/components/toggles/index.js
+++
@@ -1,2 +0,0 @@
-export {UnitSelection} from './UnitSelection';
-export {SexSelection} from './SexSelection';
