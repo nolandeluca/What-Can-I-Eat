27fdf4f3fded5442f96359ac76ff09b37aae91d1
nolandeluca <delucanolan@gmail.com>
2019-03-13 04:22:07 -0400

    corrected api call for desired output

--- date.js
+++ date.js
@@ -1,16 +1,16 @@
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth() + 1;
-var yyyy = today.getFullYear();
+let today = new Date();
+let dd = today.getDate();
 
-if (dd < 10) {
-  dd = '0' + dd;
-}
+let mm = today.getMonth()+1; 
+const yyyy = today.getFullYear();
+if(dd<10) 
+{
+    dd=`0${dd}`;
+} 
 
-if (mm < 10) {
-  mm = '0' + mm;
-}
-
-today = yyyy + '-' + mm + '-' + dd;
-
+if(mm<10) 
+{
+    mm=`0${mm}`;
+} 
+today = `${yyyy}-${mm}-${dd}`;
 console.log(today);
