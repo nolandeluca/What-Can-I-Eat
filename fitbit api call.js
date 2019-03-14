c6fc7890cc1c7b91aa5a550e4d9618bc6dbd2e46
Nolan Deluca <43088000+nolandeluca@users.noreply.github.com>
2019-03-14 19:34:30 -0400

    Update fitbit api call.js

--- fitbit api call.js
+++ fitbit api call.js
@@ -1,6 +1,6 @@
 var caloriesOut = "";
 var token =
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkRLR1ciLCJzdWIiOiI1V1RYUTciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyYWN0IHJudXQiLCJleHAiOjE1NTI2MzA1NzksImlhdCI6MTU1MjYwMTc3OX0.BhspVUGdyvk9XCHHDs1Q1Xa_bOTMm5HmHUVwafrt9kk";
+  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkRLR1ciLCJzdWIiOiI1V1RYUTciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyYWN0IHJudXQiLCJleHAiOjE1NTI2MzUxMzQsImlhdCI6MTU1MjYwNjMzNH0.nQZN4aX3ypvOGuK8o4jcdxCz4C_eJMdx8FTrk56NFUo";
 
 let today = new Date();
 let dd = today.getDate();
